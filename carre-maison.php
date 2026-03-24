968b28294edc2f1ac3eb571525568941d3ac4c2c
Melina38 <melinabascol@gmail.com>
2026-03-24 19:38:15 +0100

    Updat&ADD fiches projet

---
+++ carre-maison.php
@@ -0,0 +1,101 @@
+<?php include 'includes/header.php'?>
+
+    <body>
+        <div class="container">
+            <a href="index.php" class="bouton-retour">
+                <img src="./img/fleche-retour.svg" alt="fleche rouge qui pointe vers la gauche">
+                <h4>Retour</h4>
+            </a>
+        </div>
+        <div class="titre-div container">
+            <h2 class="titre-projet">Alternance d'un an chez Carré Maison</h2>
+            </h2>
+            <p>2025-2026</p>
+
+        </div>
+        <section class="projet-section container">
+            
+            <div class="categories-div">
+                <div class="categorie-div">
+                    <h3 class="sous-titre">Contexte</h3>
+                    <h4>
+                        Je suis actuellement en troisième et dernière année du <strong>BUT MMI</strong> , <strong>en parcours Développement Web
+                            et Dispositifs Interactifs</strong> . <br>
+                         Dans le cadre de ma formation, j'ai l'opportunité de <strong>réaliser une alternance</strong> en 
+                         entreprise pour mettre en <strong>pratique les compétences acquises</strong> au cours de ces trois 
+                         dernières années et <strong>m'enrichir de la vie en entreprise</strong> . <br>
+                         <br>
+                         <strong>Carré Maison </strong> m'a offert cette chance en m'accueillant dans leur agence conviviale. <br> <br>
+                         J'y occupe un rôle polyvalent de <strong>développeuse web</strong> , <strong>UX/UI designer</strong> et <strong>graphiste</strong>. <br> <br>
+                        Je participe principalement à la <strong>la refonte de leur site e-commerce et sa migration de Prestashop à Shopify</strong> en parfaite autonomie. <br>
+                        J'ai assuré <strong>l'ensemble du projet de A à Z</strong>, de la <strong>conception de la maquette</strong> 
+                        en cohérence avec leur <strong>identité visuelle</strong>, du <strong>développement complet du site</strong> jusqu'au déploiement. <br>
+                        En paralèle de ce projet, je m'occupe aussi de la maintenance du site actuel.
+                        <br>
+                         <br>
+                        J'apprenent beaucoup de choses, tant sur le plan professionnel que personnel. <br> 
+                        Ce projet représente un <strong>véritable challenge</strong>, car je n'avais encore jamais réalisé de site e-commerce dans son intégralité. 
+                        De plus, <strong>aucun professionnel n'était présent pour m'accompagner</strong>, ce qui m'a poussée à faire preuve 
+                        d'autonomie, de rigueur et de capacité d'adaptation. <br><br>
+                    </h4>
+                    
+                    <h3 class="sous-titre">Techno utilisées</h3>
+                        <h4>
+                            Liquid, HTML, CSS, JavaScript, JSON et Shopify.
+                        </h4>
+                
+                </div>
+                <div class="image-div">
+                    <img src="./img/pc_alternance.png" alt="écran d'ordinateur avec ecrit en cours">
+                    <div class="lien-div"> 
+                        <a class="bouton-lien" href="https://www.carre-maison.com/">Lien vers le site</a>
+                    </div>
+            
+                </div>
+            
+                
+            </div>
+                <div class="categories-div">
+                    <div class="categorie-div">
+                        <h3 class="sous-titre">Environnement & outils</h3>
+                        <h4>
+                            -Interface Shopify pour la programmation <br>
+                            -Notion pour concevoir mon planning<br>
+                            -Figma pour la conception de maquettes graphisme et communication<br>
+                            -YoorShop et OVH pour la gestion du site web<br>
+                        </h4>
+        
+                    </div>
+                    <div class="categorie-div">
+                        <h3 class="sous-titre">Mes tâches</h3>
+                        <h4>
+                            -<strong>Développement front-end du site :</strong> création et intégration de <strong>composants Liquid</strong> personnalisés 
+                            (sections et snippets)<br>
+                            -<strong>Maintenance du site web</strong><br>
+                            -<strong>Conception de wireframe et maquette figma mobile et desktop</strong><br>
+                            -<strong>Hébergement :</strong> Redirection des enregistrement DNS<br>
+                            -<strong>Communication :</strong> Aider à la conception de contenus pour les réseaux sociaux<br>
+                            -<strong>Remplacement :</strong> Remplacer la secrétaire en prenant les appels téléphoniques et en conseillant les clients lors de ses absences<br>
+                        </h4>
+        
+                    </div>
+                </div>
+                <div class="apprentissages-div">
+                    <h3 class="sous-titre">Ce que j'ai appris</h3>
+                    <h4>
+                        -Le travail en équipe : j'ai appris à collaborer efficacement avec mes collègues, à partager des idées et à résoudre des problèmes ensemble. <br>
+                        -La gestion de projet : j'ai appris à organiser mon travail, à respecter les délais et à gérer les priorités. <br>
+                        -Le développement web : j'ai approfondi mes compétences en développement web, notamment en utilisant TypeScript, Next.js, Docker et Directus. <br>
+                        -La communication : j'ai amélioré mes compétences en communication, que ce soit pour rédiger du contenu pour le site web ou pour communiquer avec mes collègues ou des clients. <br>
+                        -L'adaptabilité : j'ai appris à m'adapter à un nouvel environnement de travail et à relever les défis qui se présentent.
+
+                </div>
+            
+            
+        </section>
+        
+        <?php include 'components/mes-projets-section.php'?>
+        <?php include 'components/qui-suis-je-section.php'?>
+        <?php include 'components/contact-section.php'?>
+    </body>
+<?php include 'includes/footer.php'?>
--- components/mes-projets-section.php
+++ components/mes-projets-section.php
@@ -6,19 +6,35 @@
         <div class="buttons-filtre">
             <button class="filter-btn" data-filter="2024" onclick="filterCards('2024')">2024</button>
             <button class="filter-btn" data-filter="2025" onclick="filterCards('2025')">2025</button>
+            <button class="filter-btn" data-filter="2026" onclick="filterCards('2026')">2026</button>
             <button class="filter-btn" data-filter="enCours" onclick="filterCards('enCours')">En cours</button>
         </div>
         <div class="cards">
-            
-            <a href="multimedia.php" class="card card-1  " data-year="2024">
-                <h4>Création d'un site de réservation de matériels multimédia</h4> 
-                <p>#Dev web <br>#MYSQL #HTML #CSS #PHP</p> 
+            <a href="stage.php" class="card card-8 " data-year="2025">
+                <h4>Stage de 3 mois chez GIMS Immobilier</h4> 
+                <p>#DevWeb #TypeScript #Docker #Directus <br>#UX/UIdesigner #Communication</p>
+            </a>
+            <a href="carre-maison.php" class="card card-9 " data-year="enCours">
+                <h4>Alternance d'un an chez Carré Maison</h4> 
+                <p>#DevWeb #Shopify #Liquid #HTML&CSS <br>#UX/UIdesigner #Communication</p>
             </a>
             <a href="semaine_web.php" class="card card-2  " data-year="2025">
                 <h4>Conception d'un site vitrine sous wordpress
                 </h4> 
                 <p>#Dev web <br>#JavaScript #HTML #CSS #PHP</p> 
             </a>
+            <a href="jobpulse.php" class="card card-6 " data-year="2025">
+                <h4>Réalisation d'un site web avec Symfony</h4> 
+                <p>#Dev web <br>#Symfony #HTML #CSS #PHP</p> 
+            </a>
+            <a href="laravel.php" class="card card-10 " data-year="2026">
+                <h4>Réalisation d'un site web avec Laravel</h4> 
+                <p>#Dev web <br>#Laravel #HTML #CSS #PHP</p> 
+            </a>
+            <a href="multimedia.php" class="card card-1  " data-year="2024">
+                <h4>Création d'un site de réservation de matériels multimédia</h4> 
+                <p>#Dev web <br>#MYSQL #HTML #CSS #PHP</p> 
+            </a>            
             <a href="hazards.php" class="card card-3" data-year="2024">
                 <h4>Chargée de projet d'une agence étudiante, dans la
                     production de contenus multimédia</h4> 
@@ -32,18 +48,12 @@
                 <h4>Réalisation d'un jeu visual novel sur Unity</h4> 
                 <p>#Dev<br>#Unity #C-sharp <br></p> 
             </a>
-            <a href="jobpulse.php" class="card card-6 " data-year="2025">
-                <h4>Réalisation d'un site web avec Symfony</h4> 
-                <p>#Dev web <br>#Symfony #HTML #CSS #PHP</p> 
-            </a>
+            
             <a href="onimoji.php" class="card card-7 " data-year="2025">
                 <h4>Réalisation d'une application mobile hybride avec React</h4> 
                 <p>#Dev App <br>#React #JavaScript #CSS</p>
             </a>
-            <a href="stage.php" class="card card-8 " data-year="2025">
-                <h4>Stage de 3 mois chez GIMS Immobilier</h4> 
-                <p>#DevWeb #TypeScript #Docker #Directus <br>#UX/UIdesigner #Communication</p>
-            </a>
+            
         </div>
 
     </section>
