00ff97f04a63cdb8e2c04f44bfadd09e329c9f13
Melina38 <melinabascol@gmail.com>
2026-03-26 11:17:22 +0100

    Update fiche projet Laravel

--- laravel.php
+++ laravel.php
@@ -20,22 +20,19 @@
                     <h3 class="sous-titre">Contexte</h3>
                     <h4>
                         En deux jours, un professionnel du développement web est venue nous enseigner Laravel. <br>
-                        Individuellement, on a dû créer les bases d'un site web. Les modèles, les routes, les controlleurs et templates <br>
+                        Individuellement, on a dû créer les bases d'un site web. Les modèles, les routes, les controlleurs et templates d'un site qui regroupe des petites annonces de maisons <br>
                     </h4>
                     
                     <h3 class="sous-titre">Techno utilisées</h3>
                         <h4>
-                            Laravel, HTML, CSS, JavaScript et PHP
+                            Laravel, MYSQL, HTML, CSS, JavaScript et PHP
                             
                         </h4>
                 
                 </div>
                 <div class="image-div">
-                    <img src="./img/pc_jobPulse.png" alt="écran de pc">
-                    <div class="lien-div"> 
-                        <a class="bouton-lien" href="https://jobpulse-zza0.onrender.com/">Lien vers le site</a>
-                        <a class="bouton-lien" href="https://www.figma.com/design/FtCm6lECBcEWQXw4ZRB6y7/booster?node-id=271-3744&t=tyjbGJAJhG2rxaFb-1">Lien maquette</a>
-                    </div>
+                    <img src="./img/pc_enCours.png" alt="écran de pc">
+                    
                     
             
                 </div>
@@ -46,20 +43,19 @@
                         <h3 class="sous-titre">Outils</h3>
                         <h4>
                             -VisualStudioCode pour la programmation <br>
-                            -Notion pour s'organiser <br>
-                            -Figma pour la maquette<br>
-                            -La documentation de Symfony pour s'y initier<br>
-                            -Github pour pouvoir travailler en équipe<br>
+                            -La documentation de Laravel<br>
+                            
                         </h4>
         
                     </div>
                     <div class="categorie-div">
                         <h3 class="sous-titre">Mes tâches</h3>
                         <h4>
-                            -Veille et Réflexion sur l'univers graphique<br>
-                            -Développement du site back et front du site avec l'élaboration du crud des profils et du crud sur l'état de l'offre au niveau de l'entreprise et de l'étudiant<br>
-                            -Organisation des tâches<br>
-                            -Réalisation de la maquette dekstop<br>
+                            -Configurer le projet et la base de donnée<br>
+                            -Programmer les Models, Views & Controllers<br>
+                            -Faire un CRUD pour ajouter des offres<br>
+                            -Système de connexion avec authentification<br>
+                            -Définir les routes
 
                         </h4>
         
@@ -68,10 +64,11 @@
                 <div class="apprentissages-div">
                     <h3 class="sous-titre">Ce que j'ai appris</h3>
                     <h4> 
-                        Ce projet à été un veritable défi pour moi, car c'était ma première expérience avec Symfony, que j'ai appris en totale autonomie.
-                        J'ai appris à travailler sur un projet avec la structure de Symfony, son système de migration et de création de base de donnée.<br>
-                        J'ai également appris à créer des routes, des controllers et des templates avec Twig pour afficher les données de manière dynamique. <br>
-                        Et enfin, j'ai appris à utiliser Twig pour créer des templates et à intégrer du JavaScript dans mes pages Symfony. <br>
+                        Ce projet à été un veritable défi pour moi, car c'était ma première expérience avec Laravel, que j'ai appris en totale autonomie.
+                        J'ai appris à travailler sur un projet avec la structure de Laravel, son système de migration et de création de base de donnée.<br>
+                        J'ai également appris à créer des routes, des controllers et des templates pour afficher les données de manière dynamique. <br>
+                        Ce projet m'a permis de comprendre les bases de Laravel et de me familiariser avec ce framework. 
+                        Merci à Gabriel Bois d'avoir pris le temps de m'accompagner dans ce projet.
                     </h4>
                 </div>
             
